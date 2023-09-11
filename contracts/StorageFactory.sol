eea600f90eab0168e0b8c847dd9862e6de4dbe1f
Toshin Prince <toshinprince@gmail.com>
2023-09-11 16:57:47 +0530

    Factory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -1,3 +1,14 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.8;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArry;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArry.push(simpleStorage);
+    }
+}

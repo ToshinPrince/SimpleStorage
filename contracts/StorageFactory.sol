79dafab602c3b29f9f5dcd513e56bcc837f05ed8
Toshin Prince <toshinprince@gmail.com>
2023-09-11 17:26:53 +0530

    factory_update

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -11,4 +11,26 @@ contract StorageFactory {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArry.push(simpleStorage);
     }
+
+    function sfStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        //In order to intract with contracts
+        //we need to have
+        //1)address of the contract
+        //2) ABI of the conttract
+
+        SimpleStorage simpleStorage = simpleStorageArry[_simpleStorageIndex];
+        simpleStorage.store(_simpleStorageNumber);
+        // can use this single line insted of above 2 line of code
+        simpleStorageArry[_simpleStorageIndex].store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = simpleStorageArry[_simpleStorageIndex];
+        return simpleStorage.getFavouriteNumber();
+        // can use this single line insted of above 2 line of code
+        // return simpleStorageArry[_simpleStorageIndex].getFavouriteNumber();
+    }
 }

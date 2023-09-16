95ec2bbfb25c34ea3e2c15edba8f33884a32c181
Toshin Prince <toshinprince@gmail.com>
2023-09-16 11:37:56 +0530

    localhost

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,5 +1,12 @@
 const { task } = require("hardhat/config");
 
+//this is hardhat task
+//task are created inside of hardhat.config.js
+//but we can create task in seperate folder and import(require) it in hardhat.config.js
+//Once you import this task in hardhat.config.js, you will get new task, when you run yarn hardhat -> block-number
+// if you want to run the task run the script -> yarn hardhat block-number --network <networkname>
+//we can do same thing using scripts
+
 task("block-number", "Prints the current block number").setAction(
   // async function blockTask(taskArgs,hre){}
   // const blockTask = async function blockTask(taskArgs,hre){}
@@ -9,4 +16,5 @@ task("block-number", "Prints the current block number").setAction(
   }
 );
 
+// exporting block-number task
 module.exports = {};

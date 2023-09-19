9bf3e5105f184ef6636abb6987e9337e29ab9656
Toshin Prince <toshinprince@gmail.com>
2023-09-19 11:32:21 +0530

    hardhat-gas-report

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,12 +2,14 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("./tasks/block-number");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -26,6 +28,17 @@ module.exports = {
   solidity: "0.8.8",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    //in Order to get currency we need CoinMarketCap API
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    //You can mention any EVM supporting blockchian token to get gas related to that blockchain.
+    //you can check more in hardhat-gas-reporter npm docs.
+    token: "MATIC",
   },
 };
 
